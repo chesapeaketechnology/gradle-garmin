ec9631d40eda96eb0883b9ef4d5aef28e03e80af
jcampbell <jcampbell@ctic-inc.com>
2020-04-21 09:22:35 -0400

    added missing Optional import

--- src/main/java/com/chesapeaketechnology/gradle/plugins/garmin/tasks/BaseGarminTask.java
+++ src/main/java/com/chesapeaketechnology/gradle/plugins/garmin/tasks/BaseGarminTask.java
@@ -3,6 +3,7 @@ package com.chesapeaketechnology.gradle.plugins.garmin.tasks;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
 
